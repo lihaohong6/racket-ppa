2517f779515e1508f7b743b5de91cc4de47a48d2
Peter Li <lihaohong6@gmail.com>
2025-04-03 19:33:29 -0400

    Fix backport script

--- backport/build.sh
+++ backport/build.sh
@@ -1,6 +1,9 @@
+docker build -t ppa .
+docker run --name ppa -dit ppa
 gpg -a --export > pub.asc
 gpg -a --export-secret-keys > priv.asc
-docker build -t ppa
+docker cp pub.asc ppa:/root/pub.asc
+docker cp priv.asc ppa:/root/priv.asc
 rm *.asc
-docker run ppa --name ppa
-docker exec -it ppa bash
+docker exec -it -w /root ppa bash
+echo "Remember to clean up the ppa container and image with docker rm and docker rmi"
