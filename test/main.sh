4fadd03df2a7ada9ebb84331d599aaf0aa113711
Peter Li <lihaohong6@gmail.com>
2025-04-14 15:47:35 -0400

    Fix test script

--- test/main.sh
+++ test/main.sh
@@ -1,6 +1,24 @@
+#! /usr/bin/bash
+
+DOCKER="docker"
+
 for version in jammy noble oracular plucky; do
     tag="test-$version"
-    docker build --build-arg VERSION="$version" -t "$tag" .
-    docker start "$tag" --name "$tag"
-    docker exec -i --rm "$tag" bash -c "bash /root/test.sh"
+    $DOCKER build --build-arg VERSION="$version" -t "$tag" .
+    echo "Image $tag built"
+    # In case a container is already running (possibly from a previous
+    # failed test), stop and remove it.
+    $DOCKER rm -f "$tag"
+    $DOCKER run --name "$tag" -dit "$tag"
+    echo "Container $tag started"
+    if $DOCKER exec "$tag" bash -c "bash /root/test.sh"; then
+        echo "Test for $version passed"
+        echo "Removing container and image"
+        $DOCKER stop "$tag"
+        $DOCKER rm "$tag"
+        $DOCKER rmi "$tag"
+    else
+        echo "Test failed. Inspect container $tag for details."
+        exit 1
+    fi
 done
